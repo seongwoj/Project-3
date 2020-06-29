87d09213c233e0e80d2e7d1e6960e2e5568174ac
William Grundler <wrgrundler@gmail.com>
2020-06-29 14:17:26 -0700

    tentative validation, needs heroku mlab

--- models/user.js
+++ models/user.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose'); 
+
 
 const userSchema = new Schema({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  email: {type: String, required: true, unique:true}, 
+    username : {type: String, unique: true, required:true}, 
 });
+
+userSchema.plugin(passportLocalMongoose); 
 
 const User = mongoose.model("User", userSchema);
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const passport = require("passport");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -8,10 +8,32 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+//Passport
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser()); 
+const LocalStrategy = require('passport-local').Strategy; 
+passport.use(new LocalStrategy(User.authenticate())); 
+
+// Passport config
+require("./config/passport")(passport);
+
 // Add routes, both API and view
 app.use(routes);
 
