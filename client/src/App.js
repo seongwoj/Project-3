a027785272e9969a3bd1feba3e6d84b0dbace054
William Grundler <wrgrundler@gmail.com>
2020-07-01 14:41:40 -0700

    redux boiler plate finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 // import Users from "./pages/Users";
 // import Detail from "./pages/Detail";
 // import NoMatch from "./pages/NoMatch";
@@ -9,10 +11,12 @@ import SignUp from "./components/SignUp";
 
 function App() {
   return (
+    <Provider store={store}>
     <Router>
-      <Route exact path="/login" component={LogIn}/>
+      <Route exact path={['/','/login']} component={LogIn}/>
       <Route exact path="/signup" component={SignUp}/>
     </Router>
+    </Provider>
   );
 
 }
