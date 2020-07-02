068637c9805e5f03d304c8220ee474eafe101c55
Kaeneth Dayao <kendayao@gmail.com>
2020-07-02 12:43:50 -0700

    merging from master

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: "mongodb://user1:password1@ds027175.mlab.com:27175/heroku_5bwdt289",
+    mongoURI: "mongodb://localhost/fidoDB",
     secretOrKey: "secret"
   };
 
--- server.js
+++ server.js
@@ -29,3 +29,5 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+
