c6d57f3633ca3867b2a0694cb0499529ab1e568e
William Grundler <wrgrundler@gmail.com>
2020-07-07 01:07:35 -0700

    icon automatically renders in dashboard based on entry during signup

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,63 +1,41 @@
 import React, { Component } from "react";
 import API from "../utils/dogImgAPI";
 import Container from "./Container";
-import SearchForm from "./SearchForm";
 import SearchResults from "./SearchResults";
+import { connect } from "react-redux";
 import Alert from "./Alert";
 
 class Search extends Component {
   state = {
-    search: "",
-    breeds: [],
     results: [],
     error: ""
   };
 
  
-  // When the component mounts, get a list of all available base breeds and update this.state.breeds
   componentDidMount() {
-    API.getBaseBreedsList()
-      
-      .then(res => console.log(res.data))
-    
-      .catch(err => console.log(err));
+ 
+    API.getDogsOfBreed(this.props.auth.user.icon)
+    .then(res => {
+      if (res.data.status === "error") {
+        throw new Error(res.data.message);
+      }
+      this.setState({ results: res.data.message, error: "" });
+  
+    })
+    .catch(err => this.setState({ error: err.message }));
   }
 
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
-  };
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    API.getDogsOfBreed(this.state.search)
-    // API.getDogsOfBreed()
-    // .then(res => console.log(res.config))
-      .then(res => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-        // console.log(res.data.message[0])
-    
-      })
-      .catch(err => this.setState({ error: err.message }));
-  };
   render() {
     return (
       <div>
         <Container style={{ minHeight: "80%" }}>
-          <h1 className="text-center">Search By Breed!</h1>
           <Alert
             type="danger"
             style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
           >
             {this.state.error}
           </Alert>
-          <SearchForm
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-            breeds={this.state.breeds}
-          />
           <SearchResults results={this.state.results} />
         </Container>
       </div>
@@ -65,4 +43,14 @@ class Search extends Component {
   }
 }
 
-export default Search;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+
+
+
+export default connect(
+  mapStateToProps,
+  { SearchResults }
+)(Search);
