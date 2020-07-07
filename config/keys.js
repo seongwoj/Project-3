63a1f4ade808263fb0f953861b65c4a0c44bd120
Kaeneth Dayao <kendayao@gmail.com>
2020-07-07 15:36:31 -0700

    changed dB

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
 module.exports = {
-    mongoURI: "mongodb://localhost/fidoDB",
+    mongoURI: "mongodb://user1:password1@ds027175.mlab.com:27175/heroku_5bwdt289",
     secretOrKey: "password1"
   };
 
-// mongoURI: "mongodb://user1:password1@ds027175.mlab.com:27175/heroku_5bwdt289"
-// mongoURI: "mongodb://localhost/fidoDB"
 
