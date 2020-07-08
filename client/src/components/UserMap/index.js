5c8cbe2b1cb2c08dd92df928fb689b8fa8d14be8
Kaeneth Dayao <kendayao@gmail.com>
2020-07-07 18:01:33 -0700

    bug-fixes

--- client/src/components/UserMap/index.js
+++ client/src/components/UserMap/index.js
@@ -3,12 +3,14 @@ import API from '../../utils/API'
 import UserMapContainer from "../UserMapContainer"
 import { connect } from "react-redux";
 import Chatroom from "../Chatroom"
+import { Link,useLocation } from "react-router-dom"
+import "./styles.css"
 
 
 function UserMap(props){
 
 const [userInfo, setUserInfo]=useState(null)
-
+const location = useLocation();
     useEffect(()=>{
         API.getUserLocation().then((res)=>{
             setUserInfo(res.data)
@@ -21,19 +23,19 @@ return(
         <nav className="navbar navbar-default navbar-fidosearch">
             <div className="container-fluid">
                 <div className="navbar-header">
-                    <a className="navbar-brand logo-text" href="/fidosearch">
-                    Dog Owners Near You
-                    </a>
+                
+                <h3 id="dog-owners-title">Dog Owners</h3>
+             
                 </div>
                 <div className="navbar-header ml-auto">
-                    <a className="navbar-brand back-text" href="/main">
-                    Back to Main Page
-                    </a>
+                <Link to="/main" id="link"className={location.pathname === "/main" ? "nav-link active" : "nav-link"}>
+                Back to Main Page
+              </Link>
                 </div>
             </div>
         </nav>
     <UserMapContainer 
-    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAJ67XPqFCkQROFj98MvLbeDMnxkKsCWpM`}
           loadingElement={<div style={{ height: `100%` }} />}
           containerElement={ <div style={{ height: `300px`, width: '100%', marginTop:`2px` }} /> }
           mapElement={ <div style={{ height: `100%` }} /> }
@@ -43,7 +45,7 @@ return(
     />
     
     <Chatroom username={props.auth.user.username}/>
-    <a href="/fidosearch" className="btn btn-primary">Find Dog Friendly Places</a>
+    <Link to="/fidosearch" id="find-friendly-button" className="btn btn-primary">Find Dog Friendly Places</Link>
     </div>
 )
 
