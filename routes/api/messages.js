cc27cdcc5c22bbdefd2510a9fe255112c6e297ca
William Grundler <wrgrundler@gmail.com>
2020-07-13 11:07:31 -0700

    messages now send to DB

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -1,17 +1,17 @@
-const router = require('express').Router();
-const Message = require("../../models/Message")
+// const router = require('express').Router();
+// const Message = require("../../models/Message")
 
-router.post('/', (req, res) => {
-    const messageToPost = new Message({ 
-      name: req.body.name,
-      email: req.body.email,
-    subject: req.body.subject,
-    message: req.body.message,
+// router.post('/', (req, res) => {
+//     const messageToPost = new Message({ 
+//       name: req.body.name,
+//       email: req.body.email,
+//     subject: req.body.subject,
+//     message: req.body.message,
     
-    }); 
-    messageToPost.save() 
-      .then(item => 
-        res.json(item)) 
-  })
+//     }); 
+//     messageToPost.save() 
+//       .then(item => 
+//         res.json(item)) 
+//   })
 
-const router = require('express').Router();
+// const router = require('express').Router();
